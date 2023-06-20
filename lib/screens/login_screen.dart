1fc7caf92a753e05befa668f6fcfe81f8c5c4335
DreuDavids <dreu.davids1@gmail.com>
2023-06-20 14:21:27 +0200

    add loading spinner to login and register screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:chat_app/components/components.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 import '../constants.dart';
 import 'chat_screen.dart';
@@ -17,64 +18,77 @@ class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
   String email = '';
   String password = '';
+  bool showSpinner = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const LogoBox(height: 200),
-            const SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                //Do something with the user input.
-                email = value;
-              },
-              decoration: kidTextFieldDecoration.copyWith(
-                hintText: 'Enter Your Email',
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        blur: 3,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const LogoBox(height: 200),
+              const SizedBox(
+                height: 48.0,
               ),
-            ),
-            const SizedBox(
-              height: 28.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-                password = value;
-              },
-              obscureText: true,
-              decoration: kidTextFieldDecoration.copyWith(
-                hintText: 'Enter Your Password',
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  //Do something with the user input.
+                  email = value;
+                },
+                decoration: kidTextFieldDecoration.copyWith(
+                  hintText: 'Enter Your Email',
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            MyTonalButton(
-              title: 'Log In',
-              onTap: () async {
-                try {
-                  //log In user
-                  final user = await _auth.signInWithEmailAndPassword(
-                      email: email, password: password);
+              const SizedBox(
+                height: 28.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  //Do something with the user input.
+                  password = value;
+                },
+                obscureText: true,
+                decoration: kidTextFieldDecoration.copyWith(
+                  hintText: 'Enter Your Password',
+                ),
+              ),
+              const SizedBox(
+                height: 48.0,
+              ),
+              MyTonalButton(
+                title: 'Log In',
+                onTap: () async {
+                  //show load
+                  setState(() {
+                    showSpinner = true;
+                  });
 
-                  if (user != null && mounted) {
-                    Navigator.pushNamedAndRemoveUntil(
-                        context, ChatScreen.id, (route) => false);
+                  try {
+                    //log In user
+                    final user = await _auth.signInWithEmailAndPassword(
+                        email: email, password: password);
+
+                    if (user != null && mounted) {
+                      Navigator.pushNamedAndRemoveUntil(
+                          context, ChatScreen.id, (route) => false);
+                    }
+                  } catch (e) {
+                    debugPrint(e.toString());
                   }
-                } catch (e) {
-                  debugPrint(e.toString());
-                }
-              },
-            )
-          ],
+                  setState(() {
+                    showSpinner = false;
+                  });
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -2,6 +2,7 @@ import 'package:chat_app/components/components.dart';
 import 'package:chat_app/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 import 'chat_screen.dart';
 
@@ -17,64 +18,75 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   final _auth = FirebaseAuth.instance;
   String email = '';
   String password = '';
+  bool showSpinner = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const LogoBox(height: 200),
-            const SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                //Do something with the user input.
-                email = value;
-              },
-              decoration: kidTextFieldDecoration.copyWith(
-                hintText: 'Enter Your Email',
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        blur: 3,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const LogoBox(height: 200),
+              const SizedBox(
+                height: 48.0,
               ),
-            ),
-            const SizedBox(
-              height: 24.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-                password = value;
-              },
-              obscureText: true,
-              decoration: kidTextFieldDecoration.copyWith(
-                hintText: 'Enter Your Password',
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  //Do something with the user input.
+                  email = value;
+                },
+                decoration: kidTextFieldDecoration.copyWith(
+                  hintText: 'Enter Your Email',
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            MyTonalButton(
-              title: 'Register',
-              onTap: () async {
-                //register the user here
-                try {
-                  final newUser = await _auth.createUserWithEmailAndPassword(
-                      email: email, password: password);
+              const SizedBox(
+                height: 24.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  //Do something with the user input.
+                  password = value;
+                },
+                obscureText: true,
+                decoration: kidTextFieldDecoration.copyWith(
+                  hintText: 'Enter Your Password',
+                ),
+              ),
+              const SizedBox(
+                height: 48.0,
+              ),
+              MyTonalButton(
+                title: 'Register',
+                onTap: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  //register the user here
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                        email: email, password: password);
 
-                  if (newUser != null && mounted) {
-                    Navigator.pushNamedAndRemoveUntil(
-                        context, ChatScreen.id, (route) => false);
+                    if (newUser != null && mounted) {
+                      Navigator.pushNamedAndRemoveUntil(
+                          context, ChatScreen.id, (route) => false);
+                    }
+                  } catch (e) {
+                    debugPrint(e.toString());
                   }
-                } catch (e) {
-                  debugPrint(e.toString());
-                }
-              },
-            )
-          ],
+                  setState(() {
+                    showSpinner = false;
+                  });
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
